ad00bea20178fd25fe1222c7829d14c6bb49fd36
José <jose@gurmeo.com>
2024-10-17 16:33:52 +0200

    chore: update dependencies and configuration files
    - Updated `package-lock.json` with the latest dependencies.
    - Updated `tailwind.config.js` for JIT mode and content paths.
    - Updated `tsconfig.json` with strict compiler options and custom type roots.
    - Updated `webpack.config.js` for SCSS processing and asset handling.
    - Added `Procfile` for Heroku deployment.

--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ const router = Router();
 
 /* GET home page. */
 router.get('/', (_req: Request, res: Response, _next: NextFunction) => {
-  res.render('index', { title: 'Title' });
+  res.render('index', { title: 'Campmajó Studio - Llum i narrativa' });
 });
 
 export default router;
